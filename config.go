5e21e4d14ab7de942a3426e47f959b2680439824
tux <tux@xmission.com>
2016-03-03 17:12:13 -0700

    Update config.go to match current yml layout

--- config.go
+++ config.go
@@ -1,29 +1,34 @@
 package main
 
-import "gopkg.in/yaml.v2"
+import (
+    "gopkg.in/yaml.v2"
+    "io/ioutil"
+)
 
 type BotConfig struct {
-    Connection struct {
-        Server      string  `yaml:"server"`
-        Port        uint16  `yaml:"port"`
-        SSL         bool    `yaml:"ssl"`
-        IgnoreCert  bool    `yaml:"ignore_cert"`
-        Channels    []string   `yaml:"channels,flow"`
-        Bot struct {
-            Nick        string  `yaml:"nick"`
+    Server      string  `yaml:"server"`
+    Port        string  `yaml:"port"`
+    SSL         bool    `yaml:"ssl"`
+    IgnoreCert  bool    `yaml:"ignore_cert"`
+    Channels    []string   `yaml:"channels,flow"`
+    Bot struct {
+        Nick        string  `yaml:"nick"`
+        User        string  `yaml:"user"`
+        Nickserv struct {
+            Enabled     bool    `yaml:"enabled"`
+            Pass        string  `yaml:"pass"`
             User        string  `yaml:"user"`
-            Nickserv struct {
-                Enabled     bool    `yaml:"enabled"`
-                Pass        string  `yaml:"pass"`
-                User        string  `yaml:"user"`
-                ServName    string  `yaml:"serv_name"`
-                ServCommand string  `yaml:"serv_command"`
-            }
+            ServName    string  `yaml:"serv_name"`
+            ServCommand string  `yaml:"serv_command"`
         }
     }
 }
 
-func (c *BotConfig) Init(data []byte) error {
+func (c *BotConfig) Init(filename string) error {
+    data, err := ioutil.ReadFile(filename)
+    if err != nil {
+        panic(err)
+    }
     if err := yaml.Unmarshal(data, &c); err != nil {
         return err
     }
