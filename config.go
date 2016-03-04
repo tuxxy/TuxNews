cd45c427e5c95307890df498498219d71e441732
tux <tux@xmission.com>
2016-03-03 17:37:31 -0700

    Run 'go fmt' on config and irc

--- config.go
+++ config.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-    "gopkg.in/yaml.v2"
-    "io/ioutil"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
 
 type BotConfig struct {
-    Server      string      `yaml:"server"`
-    Port        string      `yaml:"port"`
-    SSL         bool        `yaml:"ssl"`
-    IgnoreCert  bool        `yaml:"ignore_cert"`
-    Channels    []string    `yaml:"channels,flow"`
-    admins      []string    `yaml:"admins,flow"`
-    Bot struct {
-        Nick        string  `yaml:"nick"`
-        User        string  `yaml:"user"`
-        Nickserv struct {
-            Enabled     bool    `yaml:"enabled"`
-            Pass        string  `yaml:"pass"`
-            User        string  `yaml:"user"`
-            ServName    string  `yaml:"serv_name"`
-            ServCommand string  `yaml:"serv_command"`
-        }
-    }
+	Server     string   `yaml:"server"`
+	Port       string   `yaml:"port"`
+	SSL        bool     `yaml:"ssl"`
+	IgnoreCert bool     `yaml:"ignore_cert"`
+	Channels   []string `yaml:"channels,flow"`
+	admins     []string `yaml:"admins,flow"`
+	Bot        struct {
+		Nick     string `yaml:"nick"`
+		User     string `yaml:"user"`
+		Nickserv struct {
+			Enabled     bool   `yaml:"enabled"`
+			Pass        string `yaml:"pass"`
+			User        string `yaml:"user"`
+			ServName    string `yaml:"serv_name"`
+			ServCommand string `yaml:"serv_command"`
+		}
+	}
 }
 
 func (c *BotConfig) Init(filename string) error {
-    data, err := ioutil.ReadFile(filename)
-    if err != nil {
-        panic(err)
-    }
-    if err := yaml.Unmarshal(data, &c); err != nil {
-        return err
-    }
-    return nil
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	if err := yaml.Unmarshal(data, &c); err != nil {
+		return err
+	}
+	return nil
 }
--- irc.go
+++ irc.go
@@ -1,39 +1,38 @@
 package main
 
 import (
-    "crypto/tls"
-    irc "github.com/fluffle/goirc/client"
-    "fmt"
+	"crypto/tls"
+	"fmt"
+	irc "github.com/fluffle/goirc/client"
 )
 
 type Bot struct {
-    Client  *irc.Conn
-    Config  *BotConfig
+	Client *irc.Conn
+	Config *BotConfig
 }
 
 func (b *Bot) NewBot(c *BotConfig) *Bot {
-    cfg := *irc.NewConfig(c.Bot.Nick)
-    cfg.Me.Ident = c.Bot.User
-    cfg.Me.Name = "TuxNews v1.0"
-    if c.SSL {
-        cfg.SSL = true
-        cfg.SSLConfig = &tls.Config{ServerName: c.Server,
-            InsecureSkipVerify: c.IgnoreCert}
-    } else {
-        cfg.SSL = false
-    }
-    cfg.Server = c.Server + ":" + c.Port
-    cfg.Version = "TuxNews v1.0 - https://github.com/tuxxy/TuxNews"
-    
-    Client := *irc.Client(&cfg)
-    
+	cfg := *irc.NewConfig(c.Bot.Nick)
+	cfg.Me.Ident = c.Bot.User
+	cfg.Me.Name = "TuxNews v1.0"
+	if c.SSL {
+		cfg.SSL = true
+		cfg.SSLConfig = &tls.Config{ServerName: c.Server,
+			InsecureSkipVerify: c.IgnoreCert}
+	} else {
+		cfg.SSL = false
+	}
+	cfg.Server = c.Server + ":" + c.Port
+	cfg.Version = "TuxNews v1.0 - https://github.com/tuxxy/TuxNews"
 
-    return &Bot{Client: &Client, Config: c}
+	Client := *irc.Client(&cfg)
+
+	return &Bot{Client: &Client, Config: c}
 }
 
 func (b *Bot) Connect() {
 	fmt.Println("Connecting to server...")
-    if err := b.Client.Connect(); err != nil {
-        panic(err)
-    }
+	if err := b.Client.Connect(); err != nil {
+		panic(err)
+	}
 }
